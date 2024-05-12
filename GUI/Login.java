2e987252c5222eac0a54d119de1aab5bc795ead8
Sadman Sakib <sakibsadman035@gmail.com>
2024-05-12 06:33:49 +0600

    Login page done

--- GUI/Login.java
+++ GUI/Login.java
@@ -1,31 +1,43 @@
 package GUI;
 import javax.swing.*;
+
+import entity.account.*;
+import entity.person.*;
+import entityList.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class Login extends JFrame implements ActionListener {
-    JLabel emailL, pageL;
-    JLabel userPassLabel;
-    JTextField emailT;
-    JPasswordField userPassword;
-    JButton LoginButton;
-    
     Font font = new Font("Inter", Font.PLAIN, 20);
     Font font24b = new Font("Inter", Font.BOLD, 24);
     Font font16 = new Font("Inter", Font.PLAIN, 16);
+    
+    JLabel emailL, pageL, userPassL;
+    JTextField emailT;
+    JPasswordField userPassword;
+    JButton loginBtn, resetBtn;
 
+    CustomerList customerList;
+    BankerList bankerList;
 
+    public Login(CustomerList customerList, BankerList bankerList) {
 
-    public Login() {
         super("Login");
-        this.setSize(700, 400);
-        this.setLocation(400, 300);
+        this.setSize(700, 450);
+        this.setLocation(400, 200);
         this.setLayout(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        ImageIcon img = new ImageIcon("./resources/logo.png");
+        this.setIconImage(img.getImage());
+
+        this.customerList = customerList;
+        this.bankerList = bankerList;
+
         // Label for page title 
-        pageL = new JLabel("Login");
-        pageL.setBounds(318, 20, 64, 36);
+        pageL = new JLabel("FourBit Bank");
+        pageL.setBounds(270, 20, 280, 36);
         pageL.setFont(font24b);
         this.add(pageL);
 
@@ -42,10 +54,10 @@ public class Login extends JFrame implements ActionListener {
         this.add(emailT);
 
         // Label for password 
-        userPassLabel = new JLabel("Password");
-        userPassLabel.setBounds(150, 163, 100, 25);
-        userPassLabel.setFont(font16);
-        this.add(userPassLabel);
+        userPassL = new JLabel("Password");
+        userPassL.setBounds(150, 163, 100, 25);
+        userPassL.setFont(font16);
+        this.add(userPassL);
 
         // For password field
         userPassword = new JPasswordField();
@@ -55,30 +67,52 @@ public class Login extends JFrame implements ActionListener {
         this.add(userPassword);
 
         // For login button
-        LoginButton = new JButton("Login");
-        LoginButton.setBounds(318, 280, 75, 40);
-        LoginButton.setFont(font16);
-        LoginButton.setBackground(Color.BLACK);
-        LoginButton.setForeground(Color.WHITE);
-        LoginButton.addActionListener(this);
-        this.add(LoginButton);
+        loginBtn = new JButton("Login");
+        loginBtn.setBounds(260, 250, 170, 40);
+        loginBtn.setFont(font16);
+        loginBtn.setBackground(Color.BLACK);
+        loginBtn.setForeground(Color.WHITE);
+        loginBtn.addActionListener(this);
+        this.add(loginBtn);
+
+        // For Resest password button
+        resetBtn = new JButton("Reset Password");
+        resetBtn.setBounds(260, 300, 170, 30);
+        resetBtn.setFont(font16);
+        resetBtn.setBackground(Color.BLUE);
+        resetBtn.setForeground(Color.WHITE);
+        resetBtn.addActionListener(this);
+        this.add(resetBtn);
 
         this.setVisible(true);
     }
 
  
     public void actionPerformed(ActionEvent e) {
-        if (LoginButton == e.getSource()) {
-            String name = emailT.getText();
-            String pass = String.valueOf(userPassword.getPassword());
-            if (name.equals("fourbit") && pass.equals("1234")) {
-                JOptionPane.showMessageDialog(this, "Login successful");
-                ManagerDashboard home = new ManagerDashboard(this);
+        if (loginBtn == e.getSource()) {
+            String email = emailT.getText();
+            String password = String.valueOf(userPassword.getPassword());
+            
+            if (customerList.isValid(email, password)) {
+                OnlineBanking onlineBanking = new OnlineBanking(customerList, this);
                 emailT.setText(""); 
                 userPassword.setText(""); 
                 this.setVisible(false);
-            } else {
-                // JOptionPane.showMessageDialog(this, "Invalid email or password", "Error", JOptionPane.ERROR_MESSAGE);
+            } 
+            else if (bankerList.isValid(email, password).equals("Cashier")) {
+                CashierDashboard cashierDashboard = new CashierDashboard(customerList, this);
+                emailT.setText(""); 
+                userPassword.setText(""); 
+                this.setVisible(false);
+            }
+            else if (bankerList.isValid(email, password).equals("Manager")) {
+                ManagerDashboard managerDashboard = new ManagerDashboard(customerList, this);
+                emailT.setText(""); 
+                userPassword.setText(""); 
+                this.setVisible(false);
+            }
+            else {
+                JOptionPane.showMessageDialog(this, "Login failed");
             }
         }
     }
